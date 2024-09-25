9c31c878d991db5d3c1e7b14bfeedb76d742bb4f
NikitaA-12 <abramov10nikita12@gmail.com>
2024-09-25 18:38:56 +0300

    hw-1

---
+++ homework-1/correctSentence.test.js
@@ -0,0 +1,9 @@
+import correctSentence from './correctSentence';
+
+test('returns correct sentence', () => {
+  // тестируем корректность работы функции
+
+  expect(correctSentence('greetings, friends')).toBe('Greetings, friends.');
+  expect(correctSentence('Greetings, friends')).toBe('Greetings, friends.');
+  expect(correctSentence('Greetings, friends.')).toBe('Greetings, friends.');
+});
