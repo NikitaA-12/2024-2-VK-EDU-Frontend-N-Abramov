483a39d0aa7f4e7fd133545c71da835e0e9a6190
NikitaA-12 <abramov10nikita12@gmail.com>
2024-10-05 18:25:23 +0300

    deadchat_FINAL_4.7

--- simple-chat/vite.config.js
+++ simple-chat/vite.config.js
@@ -1,4 +1,6 @@
-export default {
-  root: './simple-chat', // Указывает на папку, где находится index.html
-  base: '/2024-2-VK-EDU-Frontend-N-Abramov/', // Указывает базовый путь для деплоя на GitHub Pages
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // Указываем корень текущей папки
+  base: './', // Указываем базовый путь, который будет использоваться при сборке
+});
