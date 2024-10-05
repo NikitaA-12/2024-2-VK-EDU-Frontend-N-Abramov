43e54b6dd0162dc607a99d21ccf5fd039adeb811
NikitaA-12 <abramov10nikita12@gmail.com>
2024-10-05 16:30:59 +0300

    deadchat_FINAL_3.5

--- simple-chat/vite.config.js
+++ simple-chat/vite.config.js
@@ -1,8 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
+  root: 'src', // Указываем папку, в которой находится index.html
   build: {
-    outDir: '../dist',
+    outDir: '../dist', // Указываем выходную папку для собранного проекта
+    rollupOptions: {
+      input: {
+        main: 'index.html', // Указываем путь к вашему index.html
+      },
+    },
   },
 });
