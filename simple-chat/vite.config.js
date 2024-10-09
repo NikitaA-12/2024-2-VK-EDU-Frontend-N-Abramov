b077a62957534c7b8bdc2976fffe12e88aa46525
NikitaA-12 <abramov10nikita12@gmail.com>
2024-10-10 00:47:08 +0300

    hw_3_9

--- simple-chat/vite.config.js
+++ simple-chat/vite.config.js
@@ -3,4 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.', // Указываем корень текущей папки
   base: './', // Указываем базовый путь, который будет использоваться при сборке
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        chat: 'chat-screen.html',
+      },
+    },
+  },
 });
