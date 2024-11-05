d0d2a499d6e15c784cd1d8318523c951dbdc3e6e
NikitaA-12 <abramov10nikita12@gmail.com>
2024-11-05 05:00:07 +0300

    hw_5_05_11_4

--- react-chat/src/components/SearchBar.jsx
+++ react-chat/src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchIcon from '@mui/icons-material/Search';
-import '../styles/SearchBar.css';
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
   const [isActive, setIsActive] = useState(false);
