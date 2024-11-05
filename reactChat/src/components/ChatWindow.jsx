e6745d5fbad5b0c3ed82052f3e0533c6cbaf0709
NikitaA-12 <abramov10nikita12@gmail.com>
2024-11-05 05:51:38 +0300

    hw_5_05_11_11

--- reactChat/src/components/ChatWindow.jsx
+++ reactChat/src/components/ChatWindow.jsx
@@ -142,26 +142,21 @@ const ChatWindow = ({ activeChat, onBackClick, avatar, letter }) => {
               <span className="checkmarks">
                 {msg.read ? (
                   <svg
-                    className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall read css-120dh41-MuiSvgIcon-root"
+                    className="checkmark"
                     focusable="false"
                     aria-hidden="true"
                     viewBox="0 0 24 24"
                     data-testid="DoneAllIcon">
-                    <path
-                      d="m18 7-1.41-1.41-6.34 6.34 1.41 1.41zm4.24-1.41L11.66 16.17 7.48 12l-1.41 1.41L11.66 19l12-12zM.41 13.41 6 19l1.41-1.41L1.83 12z"
-                      className="checkmark"></path>
+                    <path d="m18 7-1.41-1.41-6.34 6.34 1.41 1.41zm4.24-1.41L11.66 16.17 7.48 12l-1.41 1.41L11.66 19l12-12zM.41 13.41 6 19l1.41-1.41L1.83 12z"></path>
                   </svg>
                 ) : (
                   <svg
-                    className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall read css-120dh41-MuiSvgIcon-root"
+                    className="checkmark"
                     focusable="false"
                     aria-hidden="true"
                     viewBox="0 0 24 24"
                     data-testid="DoneIcon">
-                    <path
-                      d="M9 16.17L4.83 12 3.41 13.41 9 19l12-12-1.41-1.41z"
-                      className="checkmark"
-                    />
+                    <path d="M9 16.17L4.83 12 3.41 13.41 9 19l12-12-1.41-1.41z" />
                   </svg>
                 )}
               </span>
