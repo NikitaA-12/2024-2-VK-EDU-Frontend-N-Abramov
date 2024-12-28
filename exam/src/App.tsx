a8a60c0e7229fd4794411468c5d489bdff5c7b41
NikitaA-12 <abramov10nikita12@gmail.com>
2024-12-28 05:04:54 +0300

    exam_28_12_5

--- exam/src/App.tsx
+++ exam/src/App.tsx
@@ -1,9 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Translator from './components/Translator/Translator';
 import HistoryPage from './components/HistoryPage';
+import { useEffect } from 'react';
 import './style/main.scss';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, [navigate]);
+
   return (
     <div className="app">
       <Routes>
