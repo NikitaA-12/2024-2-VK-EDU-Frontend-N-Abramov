8e6c15c5b57f0b22d6b0c7f08bdd1137f8fcbab2
NikitaA-12 <abramov10nikita12@gmail.com>
2024-12-28 05:20:51 +0300

    exam_28_12_7

--- exam/src/main.tsx
+++ exam/src/main.tsx
@@ -3,16 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-const basename = '/2024-2-VK-EDU-Frontend-N-Abramov/';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <Router basename={basename}>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 );
